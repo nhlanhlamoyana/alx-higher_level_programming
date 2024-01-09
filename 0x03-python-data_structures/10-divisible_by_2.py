54d4fdc06a634128936e181d0fea843460d4454f
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-01-08 17:35:29 -0800

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    length - len(my_list)
+    length = len(my_list)
     new_list = []
     for i in range(length):
         if my_list[i] % 2 == 0:
