e8bc3d0f78e62cd04c5ec4a7bd7ce8da24f09653
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-01-23 09:31:24 -0800

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -11,10 +11,12 @@ class Square:
 
 
     def __init__(self, size):
-        """Initialize a new Square.
+        """Initialize a new 
+Square.
 
 
         Args:
-        size (int): The size of the new square.
+            size (int): The 
+size of the new square.
         """
         self.__size = size
