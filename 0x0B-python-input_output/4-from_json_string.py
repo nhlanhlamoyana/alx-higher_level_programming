194089cbe18751fba9daa034a9eec3b18d670686
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-02-06 08:31:55 -0800

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defining from_json_string function"""
+
+
+
+import json
+
+
+
+def from_json_string(my_str_:
+        """return json string"""
+        return json.loads(my_str)
