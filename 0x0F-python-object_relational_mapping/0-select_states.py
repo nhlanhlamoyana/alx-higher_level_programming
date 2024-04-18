fb202fb3c6be0cec29331b7e2126bf8216a011a3
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-04-18 15:07:22 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -33,7 +33,6 @@ def list_states (username, password, database):
 
   # Example usage
   if __name__ == '__main__':
-
       username = sys.argv[1]
       password = sys.argv[2]
       database = sys.argv[3]
