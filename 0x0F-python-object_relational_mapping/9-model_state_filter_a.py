fe881257074d2eb75921fcd18b27486804e4c771
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-04-19 06:00:52 -0700

    task 9 Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,16 +2,25 @@
 
 """ Lists all State objects that contain the letter a from the dtatbase hbtn_0e_0_usa database """
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from aqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 
 if __name__ = "__main__":
-    engine = cfreate_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
+    # Create a session factory
     Session = sessionmaker(bind=engine)
+
+    # Create a session object
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+
+    #Retrieve the states with letter 'a' from the
+    # database and print its ID and name
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}:{}".format(state.id, state.name))
+    
