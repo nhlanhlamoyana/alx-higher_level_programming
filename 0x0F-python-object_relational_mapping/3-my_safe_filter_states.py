61612b30b7043ab3dbde8c312739d05a1c93dc55
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-04-19 03:48:53 -0700

    task 3 SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,41 +3,16 @@
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    # Get the command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-      host='localhost',
-      port=3306,
-      user=mysql_username,
-      passwd=mysql_password,
-      db=database_name
-      )
-
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Prepare the SQL query with placeholders
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    # Execute the query with the state name as parameter
-    cursor.execute(sql_query, (state_name,))
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows
-    print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
+    db = MySQLdb.connect(hosts="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    match = sys.argv[4]
+    c.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
     db.close()
+    
