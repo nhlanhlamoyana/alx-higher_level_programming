d1e89dbc866891dd9d7bbe113eab40a3bd2957b8
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-04-26 13:30:00 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status."""
-import urlib.request
+import urllib.request
 
 
 if __name__ == "__main__":
-    request = urlib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urlib.request.urlopen(request) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode("utf-8i")))
